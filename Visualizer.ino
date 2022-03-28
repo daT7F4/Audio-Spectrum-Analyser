354e7a6ab3c435c281af3398539a77a5b20900a7
T7F4 <68499962+jansaternus@users.noreply.github.com>
2022-03-28 19:11:53 +0200

    Update Visualizer.ino

--- Visualizer.ino
+++ Visualizer.ino
@@ -1,21 +1,17 @@
 //ORIGINAL CODE: https://create.arduino.cc/projecthub/mircemk/diy-fft-audio-spectrum-analyzer-ca2926
-
 #include "U8glib.h"
 #include "fix_fft.h"
-
-#define LINEY 50
-#define LINEXL 0
-#define LINEXR 128
 
 #define SAMPLES 128
 
 #define AUDIO A0
 
-U8GLIB_KS0108_128 lcd(4,5,6,7,8,9,10,11,A5,A1,A2,A4,A3); //If you are using a KS0108 GLCD
+U8GLIB_KS0108_128 lcd(4,5,6,7,8,9,10,11,A5,A1,A2,A4,A3); // serial use, PSB = GND
 
 char im[SAMPLES];
 char data[SAMPLES];
 int barht[SAMPLES];
+int peaks[SAMPLES];
 
 void setup()
 {
@@ -31,26 +27,30 @@ void loop()
 
   for(i = 0; i < SAMPLES; i++)
   {
-    val = analogRead(AUDIO); 
+    val = analogRead(AUDIO);
     data[i] = (char)(val/4 - 128);
     im[i] = 0;
   }
 
 
+ 
   fix_fft(data, im, 7, 0);
 
+ 
   for(i = 0; i < SAMPLES/2; i++)
   {
     barht[i] = (int)sqrt(data[i] * data[i] + im[i] * im[i]);
   }
   
-  for(i = 0, j = 0; i < SAMPLES/2; i++, j += 2)
+  for(i = 0, j = 0; i < SAMPLES / 2; i++, j ++)
   {
     barht[i] = barht[j] + barht[j + 1];
   }
   
-  barchart(SAMPLES/4, barht);
+ 
+  barchart(64, barht);
 } 
+
 
 void barchart(int n, int bh[])
 {
@@ -63,18 +63,18 @@ void barchart(int n, int bh[])
   do
   {
     lcd.setFont(u8g_font_helvR08);
-    lcd.drawStr(20, 10, "FFT Audio Spectrum");
-    lcd.drawLine(LINEXL, LINEY, LINEXR, LINEY);
-    lcd.drawStr(0, LINEY + 10, "0");
-    lcd.drawStr(29, LINEY + 10, "1k");
-    lcd.drawStr(59, LINEY + 10, "2k");
-    lcd.drawStr(91, LINEY + 10, "3k");
-    lcd.drawStr(115, LINEY + 10, "Hz");
-    lcd.drawLine(0, 50, 0, 0);
+    lcd.drawStr(30, 10, "Audio Spectrum");
+    lcd.drawLine(0, 63, 128, 63);
+    lcd.drawLine(0, 63, 0, 0);
+    lcd.drawLine(0, 0, 128, 0);
+    lcd.drawLine(127, 0, 127, 63);
     
     for(i = 0; i < n; i++)
     {
-      lcd.drawBox(LINEXL + s * i - 1, LINEY - bh[i], w, bh[i] + 1); // u8glib doesn't accept box height of 0
+      if(64 - bh[i] < 1){
+      } else{
+        lcd.drawLine(i * 2, 64, i * 2, 64 - bh[i]);
+      }
     }
       
   }while(lcd.nextPage()); 
